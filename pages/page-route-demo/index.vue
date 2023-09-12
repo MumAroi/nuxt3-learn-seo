afa444e66a910693c776c2b319f5fda00b8c2068
Paramas <paramas.w@livinginsider.com>
2023-09-12 13:45:33 +0700

    refactor: ♻️ add sample route

---
+++ pages/page-route-demo/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    Hello page route demo
+    <br/>
+    <NuxtLink to="/living_en/page-route-demo/with-q-string" >Link</NuxtLink>
+    <br/>
+    <NuxtLink :to="{path:'/living_en/page-route-demo/with-q-string', query:{name: 'living-post', type:'doc'}}" >Link</NuxtLink>
+    <br/>
+    <button @click="navigate">
+      Link with query string
+    </button>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+const name = ref('living-post');
+const type = ref(1);
+
+function navigate(){
+  return navigateTo({
+    path: '/living_en/page-route-demo/with-q-string',
+    query: {
+      name: name.value,
+      type: type.value
+    }
+  })
+}
+</script>
---
+++ pages/page-route-demo/with-q-string.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    Hello with query string
+  </div>
+</template>
